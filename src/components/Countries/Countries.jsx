77cf394e4291a9e7636d87dab42f39abbd805b3c
ayeshaafifa2025 <ayeshaafifa2025@gmail.com>
2025-09-19 14:05:53 +0600

    first commit

---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <div>
+                <h1>in the countries</h1>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Countries;
